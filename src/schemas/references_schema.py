824201c322156b8a301312207454225d080a19a1
ikoposhilov <ikoposhilov@yandex.ru>
2025-04-21 23:24:05 +0500

    references for items added

--- src/schemas/references_schema.py
+++ src/schemas/references_schema.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, UUID4
 
 
 class CategorySchema(BaseModel):
+    id: UUID4
     name: str
-    description: str | None
 
     model_config = ConfigDict(from_attributes=True)
